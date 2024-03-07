c27f0c394421c1822ff118d4233b4fc4a0590334
Tsilavina <andriantsoaoniniaina@gmail.com>
2024-03-07 12:12:05 +0300

    start new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Component from './Component.jsx';
-import Application from './page/Application.jsx'
 
 class Home extends React.Component {
     render() {
         return (
             <>
-            <Application></Application>
+            
             </>
         );
     }
