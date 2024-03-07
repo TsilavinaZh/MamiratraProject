76dd82fe9ecb406df3f3468fea39510c8f9104a1
Tsilavina <andriantsoaoniniaina@gmail.com>
2024-03-07 06:49:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Component from './Component.jsx';
+import Application from './page/Application.jsx'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <>
+            <Application></Application>
+            </>
+        );
+    }
+}
+
+export default Home;
+
+
+
+
+
+
